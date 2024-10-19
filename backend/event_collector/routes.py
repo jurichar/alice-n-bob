d325259aefcace49e1b9f1cea21ab34354f47108
Julien RICHARD <jurichar@student.42.fr>
2024-10-19 12:25:55 +0200

    pytest added

--- backend/event_collector/routes.py
+++ backend/event_collector/routes.py
@@ -3,13 +3,13 @@ route.py is the file that contains the routes from SQLAlchemy ORM methods in the
 """
 
 from fastapi import APIRouter
-from schemas import EventResponse, Event, DeliveryResponse, DeliveryState
+from schemas import EventResponse, EventCreate, DeliveryResponse, DeliveryState
 
 router = APIRouter()
 
 
 @router.post("/deliveries/{id}/events", response_model=EventResponse)
-async def root(id: str, event: Event):
+async def root(id: str, event: EventCreate):
     print(f"Delivery {id} transitioned to {event.type}")
     return {"message": "ok"}
 
@@ -17,4 +17,8 @@ async def root(id: str, event: Event):
 # @router.post("/deliveries/{delivery_id}/events", response_model=EventResponse) ok
 # @router.get("/deliveries", response_model=list[DeliveryResponse])
 # @router.get("/deliveries/{delivery_id}/events", response_model=list[EventResponse])
-# @router.get("/counts")
+
+
+@router.get("/counts")
+async def counts():
+    return {"message": "ok"}
--- backend/event_collector/tests.py
+++ backend/event_collector/tests.py
@@ -1,3 +1,46 @@
 """
 tests.py is the file that contains the tests for the FastAPI application.
 """
+
+import os
+import dotenv
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from config import Base, init_db
+from main import app
+
+
+"""
+------------ Test setup ------------
+"""
+
+dotenv.load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture(scope="function")
+def db():
+    init_db()
+    session = SessionLocal()
+    yield session
+    session.close()
+    Base.metadata.drop_all(bind=engine)
+
+
+client = TestClient(app)
+
+
+"""
+------------ Tests ------------
+"""
+
+
+def test_counts(db):
+    response = client.get("/counts")
+    assert response.status_code == 200
+    assert response.json() == {"message": "ok"}
