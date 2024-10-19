d7dc18db8c13de09840f55c583f28c92f3630ad6
Julien RICHARD <jurichar@student.42.fr>
2024-10-19 11:56:04 +0200

    Add functions file and add routes

---
+++ backend/event_collector/funcs.py
@@ -0,0 +1,41 @@
+"""
+crud.py is the file that contains the CRUD operations (Create Read Update Delete).
+Only use SQLAlchemy ORM methods in this file.
+"""
+
+from sqlalchemy.orm import Session
+
+
+"""
+------------ DELIVERY PART ------------
+"""
+
+
+def create_delivery():
+    pass
+
+
+def get_deliveries():
+    pass
+
+
+"""
+------------ EVENTS PART ------------
+"""
+
+
+def get_events():
+    pass
+
+
+def create_event():
+    pass
+
+
+"""
+------------ OTHER ------------
+"""
+
+
+def get_counts():
+    pass
--- backend/event_collector/routes.py
+++ backend/event_collector/routes.py
@@ -2,7 +2,19 @@
 route.py is the file that contains the routes from SQLAlchemy ORM methods in the CRUD operations.
 """
 
-@app.post("/deliveries/{id}/events")
+from fastapi import APIRouter
+from schemas import EventResponse, Event, DeliveryResponse, DeliveryState
+
+router = APIRouter()
+
+
+@router.post("/deliveries/{id}/events", response_model=EventResponse)
 async def root(id: str, event: Event):
     print(f"Delivery {id} transitioned to {event.type}")
     return {"message": "ok"}
+
+
+# @router.post("/deliveries/{delivery_id}/events", response_model=EventResponse) ok
+# @router.get("/deliveries", response_model=list[DeliveryResponse])
+# @router.get("/deliveries/{delivery_id}/events", response_model=list[EventResponse])
+# @router.get("/counts")
