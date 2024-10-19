bacc837405b65c8043dbdc1184f40cac54571993
Julien RICHARD <jurichar@student.42.fr>
2024-10-19 15:19:04 +0200

    Refactor Dashboard component to use Material-UI components and improve data fetching

--- frontend/drone-delivery-frontend/src/components/Dashboard.js
+++ frontend/drone-delivery-frontend/src/components/Dashboard.js
@@ -1,32 +1,57 @@
 // Dashboard.js
 
-import { useEffect, useState } from 'react';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import { useCallback, useEffect, useState } from 'react';
 
 const Dashboard = () => {
   const [deliveries, setDeliveries] = useState([]);
   const [loading, setLoading] = useState(true);
   
-  useEffect(() => {
-    const fetchDeliveries = async () => {
-      const response = await fetch('http://localhost:8000/deliveries');
+  const fetchDeliveries = useCallback(async () => {
+    try {
+      const response = await fetch("http://localhost:8000/deliveries");
+      if (!response.ok) {
+        throw new Error("Failed to fetch deliveries");
+      }
       const data = await response.json();
       setDeliveries(data);
-      setLoading(false);
-    };
-    fetchDeliveries();
+    } catch (error) {
+      console.error("Error fetching deliveries", error);
+    } finally {
+      setLoading(false)
+    }
   }, []);
+
+  useEffect(() => {
+    fetchDeliveries();
+  }, [fetchDeliveries]);
 
   return (
     <div>
-      <h1>Dashboard</h1>
+      <Typography variant="h4" gutterBottom>
+        Ongoing Deliveries
+      </Typography>
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <ul>
-          {deliveries.map((delivery) => (
-            <li key={delivery.id}>{delivery.address}</li>
-          ))}
-        </ul>
+        <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell><strong>Status</strong></TableCell>
+              <TableCell><strong>Delivery ID</strong></TableCell>
+              <TableCell><strong>Actions</strong></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell colSpan={5} align="center">
+                No deliveries
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
       )}
     </div>
   );
