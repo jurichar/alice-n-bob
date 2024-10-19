d0a01c046980595d9728dcf6283770e981ee74ce
Julien RICHARD <jurichar@student.42.fr>
2024-10-19 11:48:07 +0200

    add schemas from models

--- backend/event_collector/models.py
+++ backend/event_collector/models.py
@@ -2,14 +2,14 @@
 models.py is the file that contains the models for SQLAlchemy ORM.
 """
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import declarative_base
-import enum
+from enum import Enum
 
 Base = declarative_base()
 
 
-class DeliveryState(str, enum.Enum):
+class DeliveryState(str, Enum):
     PARCEL_COLLECTED = "PARCEL_COLLECTED"
     TAKEN_OFF = "TAKEN_OFF"
     LANDED = "LANDED"
@@ -21,7 +21,7 @@ class Delivery(Base):
     __tablename__ = "deliveries"
 
     id = Column(String, primary_key=True, default="1")
-    state = Column(enum.Enum(DeliveryState))
+    state = Column(Enum(DeliveryState))
     created_at = Column(String)
     updated_at = Column(String)
 
--- backend/event_collector/schemas.py
+++ backend/event_collector/schemas.py
@@ -3,7 +3,31 @@ schemas.py is the file that contains the schemas for Pydantic models.
 """
 
 from pydantic import BaseModel
+from enum import Enum
+
+
+class DeliveryState(str, Enum):
+    PARCEL_COLLECTED = "PARCEL_COLLECTED"
+    TAKEN_OFF = "TAKEN_OFF"
+    LANDED = "LANDED"
+    CRASHED = "CRASHED"
+    PARCEL_DELIVERED = "PARCEL_DELIVERED"
 
 
 class Event(BaseModel):
     type: str
+
+
+class DeliveryResponse(BaseModel):
+    id: str
+    state: DeliveryState
+    created_at: str
+    updated_at: str
+
+
+class EventResponse(BaseModel):
+    id: str
+    delivery_id: str
+    type: str
+    created_at: str
+    updated_at: str
