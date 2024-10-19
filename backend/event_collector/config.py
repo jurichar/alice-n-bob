cf118d4a251575053ff3640c739c19ce59aa5c4e
Julien RICHARD <jurichar@student.42.fr>
2024-10-19 11:44:37 +0200

    add sql alchemy

--- backend/event_collector/config.py
+++ backend/event_collector/config.py
@@ -1,3 +1,20 @@
 """
 config.py is the file that contains the configuration.
 """
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
--- backend/event_collector/models.py
+++ backend/event_collector/models.py
@@ -1,3 +1,36 @@
 """
 models.py is the file that contains the models for SQLAlchemy ORM.
 """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+import enum
+
+Base = declarative_base()
+
+
+class DeliveryState(str, enum.Enum):
+    PARCEL_COLLECTED = "PARCEL_COLLECTED"
+    TAKEN_OFF = "TAKEN_OFF"
+    LANDED = "LANDED"
+    CRASHED = "CRASHED"
+    PARCEL_DELIVERED = "PARCEL_DELIVERED"
+
+
+class Delivery(Base):
+    __tablename__ = "deliveries"
+
+    id = Column(String, primary_key=True, default="1")
+    state = Column(enum.Enum(DeliveryState))
+    created_at = Column(String)
+    updated_at = Column(String)
+
+
+class Event(Base):
+    __tablename__ = "events"
+
+    id = Column(String, primary_key=True, autoincrement=True)
+    delivery_id = Column(String)
+    type = Column(String)
+    created_at = Column(String)
+    updated_at = Column(String)
